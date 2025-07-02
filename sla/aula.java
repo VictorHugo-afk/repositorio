3917d3759d3240bfabd84d56b30f7747be55b394
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2025-07-02 08:22:20 -0300

    sei
    package pasta;
    
    public class app2 {
    
        public static void main(String[] args) {
            aula livro = new aula ();
    
            livro.nome = "O senhor dos aneis";
            livro.descricao = "Uma obra literaria de fantasia";
            livro.isbn = "2710";
            livro.preco = 30.90;
            livro.autor = "j.r.r. tolkien";
            livro.datapub = "1954";
    
            System.out.println(livro.nome);
        }
    }

---
+++ sla/aula.java
@@ -0,0 +1,13 @@
+package sla;
+
+
+public class aula {
+    public static void main(String[] args) {
+    String nome;
+    String descricao;
+    String autor;
+    String isbn;
+    double preco;
+    String datapub;
+}
+}
